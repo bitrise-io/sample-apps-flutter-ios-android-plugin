eecb853d059abb980ad291a4d964ea086f2682d1
Papik Tamas <trapacska@gmail.com>
2018-12-21 13:52:12 +0100

    first commit

---
+++ ios/Classes/PlugintestPlugin.m
@@ -0,0 +1,20 @@
+#import "PlugintestPlugin.h"
+
+@implementation PlugintestPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"plugintest"
+            binaryMessenger:[registrar messenger]];
+  PlugintestPlugin* instance = [[PlugintestPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
